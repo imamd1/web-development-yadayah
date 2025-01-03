bb0678710b1fd3888224fc5324a651109a537c81
Djoko Imam <sakurakun002@gmail.com>
2025-01-04 01:14:19 +0700

    index selesai semua

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-AOS.init();
+var repeat = function(activeClass) {
+    let active = document.
+                 getElementsByClassName("active");
+  
+    let i = 1;
+    var repeater = function() {
+      setTimeout(function() {
+         [...active].forEach(function(activeSlide) {
+            activeSlide.classList.remove("active");
+         });
+  
+         slides[i].classList.add("active");
+         buttons[i].classList.add("active");
+         i++
+  
+         if(slides.length == i) {
+           i = 0;
+         }
+         if(i >= slides.length) {
+           return;
+         }
+         repeater(); 
+       }, 5000);
+    } 
+    repeater();
+  }
+  repeat();
+  AOS.init();
